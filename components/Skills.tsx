e37f0782e7aac3f1b4dc8e44636ec7cb33d5ffd4
YahyaMHD <142099296+YahyaMHD@users.noreply.github.com>
2023-08-30 18:28:44 +0200

    Deployed website

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,56 +1,49 @@
-"use client"
-import React from "react";
-import { skillsData } from "@/lib/data";
+"use client";
 
+import React from "react";
+import SectionHeading from "./section-heading";
+import { skillsData } from "@/lib/data";
 import { motion } from "framer-motion";
 
-
-  const fadeInAnimationVariants = {
-    initial: {
-      opacity: 0,
-      y: 100,
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
     },
-    animate: (index: number) => ({
-      opacity: 1,
-      y: 0,
-      transition: {
-        delay: 0.05 * index,
-      },
-    }),
-  };
+  }),
+};
 
-
-const Skills = () => {
-
-
+export default function Skills() {
 
   return (
-    <div id="Skills" className="scroll-m-20">
-      <div  className='text-title-gradient text-[40px] font-bold text-center mb-10' >
-        My Skills
-      </div>
-<section className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40 font-bold sm:w-[400px] w-[300px]'>
-    <ul className='flex flex-wrap justify-center gap-2 '>
+    <section
+      id="skills"
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+    >
+      <SectionHeading>My skills</SectionHeading>
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
-
-<motion.li
-className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
-key={index}
-variants={fadeInAnimationVariants}
-initial="initial"
-whileInView="animate"
-viewport={{
-  once: true,
-}}
-custom={index}
->
-{skill}
-</motion.li>
+          <motion.li
+            className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
         ))}
-    </ul>
-</section>
-</div>
-  )
+      </ul>
+    </section>
+  );
 }
-
-export default Skills
